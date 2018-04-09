5363e7bb92ac9bb901cf876c3a6ab34f6c9d3623
User <User@User-PC>
2018-04-09 12:24:50 +0300

    balance

--- usb/sources/sourcesCW/answer.h
+++ usb/sources/sourcesCW/answer.h
@@ -2,7 +2,7 @@
 typedef unsigned char BYTE;
 //typedef unsigned int uint32_t;
 
-int dataToBuffer(uint8_t *dataIn, uint32_t *lenIn, uint8_t *buffer, uint32_t *lenBuf, uint32_t *send, int *pinInit);
-void answerCom(uint8_t *dataIn, uint32_t* lenIn, uint8_t *dataOut, uint32_t* lenOut, int *pinInit);
-void checkPin(int *pinInit);
-void sign (int id, int amount, BYTE *mess, int valueTr, BYTE *addr, BYTE *out, uint32_t *lenOut, int *pinInit);
+int dataToBuffer(uint8_t *dataIn, uint32_t *lenIn, uint8_t *buffer, uint32_t *lenBuf, uint32_t *send, int *pinInit, uint32_t *pinDef);
+void answerCom(uint8_t *dataIn, uint32_t* lenIn, uint8_t *dataOut, uint32_t* lenOut, int *pinInit, uint32_t *pinDef);
+void checkPin(int *pinInit, uint32_t *pinDef);
+void sign (int id, int amount, BYTE *mess, int valueTr, BYTE *addr, BYTE *out, uint32_t *lenOut, int *pinInit, uint32_t *pinDef);
