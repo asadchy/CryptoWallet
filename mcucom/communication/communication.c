cc194328a5ba676a7d6ace75cefaac6e57b2892f
Artyom Asadchy <111@gmail.com>
2018-09-23 22:42:59 +0300

    Add new commands parsing

--- mcucom/communication/communication.c
+++ mcucom/communication/communication.c
@@ -11,6 +11,7 @@
 #include "queue.h"
 
 #include "stdint.h"
+#include "stdio.h"
 #include "communication.h"
 #include "i2c.h"
 #include "data.hpp"
@@ -20,10 +21,12 @@ SemaphoreHandle_t packet_ready_sem;
 struct ipc_packet tx_packet, rx_packet;
 struct ipc_wrapper wrp;
 
-struct message mess_to_lcd, mess_from_lcd;
+static struct message mess_to_lcd, mess_from_lcd;
+uint8_t status = 0xFF;
 uint32_t pin = 0xFFFFFFFF;
 uint32_t attempts_left;
 static struct wallet_status currency_info;
+static struct transaction trans;
 
 void packet_parser();
 
@@ -59,6 +62,30 @@ void Communication_Task(void *params)
 	wrp.tx = &tx_packet;
 	wrp.rx = &rx_packet;
 	wrp.sem = &packet_ready_sem;
+
+	currency_info.curr[0].curr_name = BTC;
+	currency_info.curr[0].amount_dollars[0] = '0';
+	currency_info.curr[0].amount_dollars[1] = 0;
+	currency_info.curr[0].amount[0] = '0';
+	currency_info.curr[0].amount[1] = 0;
+
+	currency_info.curr[1].curr_name = ETH;
+	currency_info.curr[1].amount_dollars[0] = '0';
+	currency_info.curr[1].amount_dollars[1] = 0;
+	currency_info.curr[1].amount[0] = '0';
+	currency_info.curr[1].amount[1] = 0;
+
+	currency_info.curr[2].curr_name = LTC;
+	currency_info.curr[2].amount_dollars[0] = '0';
+	currency_info.curr[2].amount_dollars[1] = 0;
+	currency_info.curr[2].amount[0] = '0';
+	currency_info.curr[2].amount[1] = 0;
+
+	trans.curr_name = BTC;
+	memcpy(trans.addr, "0x123456789ABCDEF", sizeof("0x123456789ABCDEF"));
+	memcpy(trans.value, "100.500", sizeof("100.500"));
+
+	currency_info.num = 3;
 
 	BOARD_InitI2C(&wrp);
 
@@ -117,6 +144,7 @@ void packet_parser()
 {
 	if(rx_packet.data[0] == 0x12)
 	{
+		uint32_t crypto, crypto_fract;
 		uint32_t data_len = rx_packet.data[2];
 		uint32_t data_header_len = data_len + 3;
 		uint16_t calc_crc, rx_crc = (((uint16_t)rx_packet.data[data_header_len]) << 8) | rx_packet.data[data_header_len + 1];
@@ -130,27 +158,29 @@ void packet_parser()
 			switch(cmd)
 			{
 			case WALLET_ENTER_PIN:
-				mess_to_lcd.cmd = WALLET_ENTER_PIN;
+			case WALLET_SET_PIN:
+			case WALLET_ENTER_MS:
+			case WALLET_SET_MS:
+			case WALLET_INIT:
+			case WALLET_BLOCKED:
+				mess_to_lcd.cmd = cmd;
 				xQueueSend(card_to_lcd, (void* )&mess_to_lcd, 0);
 
 				memset(tx_packet.data, 0, sizeof(tx_packet.data));
 				tx_packet.data[0] = 0x21;
 				tx_packet.data[1] = cmd;
-				tx_packet.data[2] = 0x04;
-				tx_packet.data[3] = pin >> 24;
-				tx_packet.data[4] = pin >> 16;
-				tx_packet.data[5] = pin >> 8;
-				tx_packet.data[6] = pin;
+				tx_packet.data[2] = 0x00;
 				tx_crc = crc16(tx_packet.data, tx_packet.data[2] + 3);
-				tx_packet.data[7] = tx_crc >> 8;
-				tx_packet.data[8] = tx_crc;
-				pin = 0xFFFFFFFF;
+				tx_packet.data[3] = tx_crc >> 8;
+				tx_packet.data[4] = tx_crc;
 
 				tx_packet.ready = true;
 				break;
 
-			case WALLET_BLOCKED:
-				mess_to_lcd.cmd = WALLET_BLOCKED;
+			case WALLET_STATUS:
+			case TRANSACTION_CONFIRMED:
+				mess_to_lcd.cmd = cmd;
+				mess_to_lcd.data = &currency_info;
 				xQueueSend(card_to_lcd, (void* )&mess_to_lcd, 0);
 
 				memset(tx_packet.data, 0, sizeof(tx_packet.data));
@@ -181,8 +211,45 @@ void packet_parser()
 				tx_packet.ready = true;
 				break;
 
-			case WALLET_STATUS:
-				mess_to_lcd.cmd = WALLET_STATUS;
+			case WALLET_CURRENCY_INFO:
+				;
+				uint8_t arg = rx_packet.data[3];
+				crypto = ((uint32_t)rx_packet.data[4] << 24) | ((uint32_t)rx_packet.data[5] << 16) |
+						((uint32_t)rx_packet.data[6] << 8) | ((uint32_t)rx_packet.data[7]);
+				crypto_fract = ((uint32_t)rx_packet.data[8] << 24) | ((uint32_t)rx_packet.data[9] << 16) |
+						((uint32_t)rx_packet.data[10] << 8) | ((uint32_t)rx_packet.data[11]);
+				uint32_t dollars = ((uint32_t)rx_packet.data[12] << 24) | ((uint32_t)rx_packet.data[13] << 16) |
+						((uint32_t)rx_packet.data[14] << 8) | ((uint32_t)rx_packet.data[15]);
+				uint32_t dollars_fract = ((uint32_t)rx_packet.data[16] << 24) | ((uint32_t)rx_packet.data[17] << 16) |
+						((uint32_t)rx_packet.data[18] << 8) | ((uint32_t)rx_packet.data[19]);
+				/* fill struct wallet_status currency_info */
+				currency_info.curr[arg].curr_name = arg;
+				snprintf(currency_info.curr[arg].amount, 16, "%u.%u", crypto, crypto_fract);
+				snprintf(currency_info.curr[arg].amount_dollars, 16, "%u.%u", dollars, dollars_fract);
+				memset(tx_packet.data, 0, sizeof(tx_packet.data));
+				tx_packet.data[0] = 0x21;
+				tx_packet.data[1] = cmd;
+				tx_packet.data[2] = 0x01;
+				tx_packet.data[2] = arg;
+				tx_crc = crc16(tx_packet.data, tx_packet.data[2] + 3);
+				tx_packet.data[4] = tx_crc >> 8;
+				tx_packet.data[5] = tx_crc;
+
+				tx_packet.ready = true;
+				break;
+
+			case WALLET_TRANSACTION:
+				/*
+				trans.curr_name = rx_packet.data[3];
+				memcpy(trans.addr, &rx_packet.data[4], 43);
+				crypto = ((uint32_t)rx_packet.data[47] << 24) | ((uint32_t)rx_packet.data[48] << 16) |
+						((uint32_t)rx_packet.data[49] << 8) | ((uint32_t)rx_packet.data[50]);
+				crypto_fract = ((uint32_t)rx_packet.data[51] << 24) | ((uint32_t)rx_packet.data[52] << 16) |
+						((uint32_t)rx_packet.data[53] << 8) | ((uint32_t)rx_packet.data[54]);
+				snprintf(trans.value, 16, "%u.%u", crypto, crypto_fract);
+				*/
+				mess_to_lcd.cmd = WALLET_TRANSACTION;
+				mess_to_lcd.data = &trans;
 				xQueueSend(card_to_lcd, (void* )&mess_to_lcd, 0);
 
 				memset(tx_packet.data, 0, sizeof(tx_packet.data));
@@ -192,23 +259,6 @@ void packet_parser()
 				tx_crc = crc16(tx_packet.data, tx_packet.data[2] + 3);
 				tx_packet.data[3] = tx_crc >> 8;
 				tx_packet.data[4] = tx_crc;
-
-				tx_packet.ready = true;
-				break;
-
-			case WALLET_CURRENCY_INFO:
-				;
-				uint8_t arg = rx_packet.data[3];
-				/* fill struct wallet_status currency_info */
-
-				memset(tx_packet.data, 0, sizeof(tx_packet.data));
-				tx_packet.data[0] = 0x21;
-				tx_packet.data[1] = cmd;
-				tx_packet.data[2] = 0x01;
-				tx_packet.data[2] = arg;
-				tx_crc = crc16(tx_packet.data, tx_packet.data[2] + 3);
-				tx_packet.data[4] = tx_crc >> 8;
-				tx_packet.data[5] = tx_crc;
 
 				tx_packet.ready = true;
 				break;
